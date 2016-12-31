f5c37631ef26b9bf5d14043679fc70fc8e531cb3
Max Ivanov <maxlvan@yandex.ru>
2016-12-31 03:22:44 +0300

    Настроил форматирование исходного html. Теперь в нем можно разбираться

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 'use strict';
 const gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    beautify = require('gulp-html-beautify');
 
 gulp.task('connect', function(){
     connect.server({
         root: 'build/'
     });
-})
+});
+
+gulp.task('beauty', function(){
+    return gulp.src(['./raw/**/*.html'])
+            .pipe(beautify({preserve_newlines: false}))
+            .pipe(gulp.dest('./build/'))
+});
+
+gulp.task('cp_all', function(){
+    return gulp.src(['./raw/**/*', '!./raw/**/*.html'])
+            .pipe(gulp.dest('./build/'))
+});
+
+gulp.task('beautify', ['beauty', 'cp_all']);
 
 gulp.task("default", ['connect']);
 
---
+++ raw/js/my_java.js
@@ -0,0 +1,105 @@
+// JavaScript Document
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "449px";
+    document.getElementById("main").style.transform = "translateX(-449px)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.transform = "translateX(0)";
+}
+
+
+function openNav_2() {
+    document.getElementById("mySidenav 2").style.width = "449px";
+  document.getElementById("main").style.transform = "translateX(-449px)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav_2() {
+    document.getElementById("mySidenav 2").style.width = "0";
+    document.getElementById("main").style.transform = "translateX(0)";
+}
+
+function openNav_3() {
+    document.getElementById("mySidenav 3").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav_3() {
+    document.getElementById("mySidenav 3").style.width = "0";
+}
+
+
+function openNav_4() {
+    document.getElementById("mySidenav 4").style.width = "449px";
+    document.getElementById("main").style.transform = "translateX(-449px)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav_4() {
+    document.getElementById("mySidenav 4").style.width = "0";
+    document.getElementById("main").style.transform = "translateX(0)";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
+
+
+
+/*SLIDER MAIN CARD*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var sticksl = document.getElementsByClassName("sticksl");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < sticksl.length; i++) {
+      sticksl[i].className = sticksl[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  sticksl[slideIndex-1].className += " active";
+}
+
+
+
+/*UP BUTTON*/
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
