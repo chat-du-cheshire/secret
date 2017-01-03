39b1f2d8be45b0513bc0c8a12652c14c4b8be014
Max Ivanov <maxlvan@yandex.ru>
2017-01-03 18:35:25 +0300

    Tops page

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 const gulp = require('gulp'),
     connect = require('gulp-connect'),
-    beautify = require('gulp-html-beautify');
+    beautify = require('gulp-html-beautify'),
+    prettify = require('gulp-html-prettify');;
 
 gulp.task('connect', function(){
     connect.server({
@@ -18,6 +19,13 @@ gulp.task('beauty', function(){
 gulp.task('cp_all', function(){
     return gulp.src(['./raw/**/*', '!./raw/**/*.html'])
             .pipe(gulp.dest('./build/'))
+});
+
+gulp.task('pretty', function(){
+    return gulp.src(['./raw/**/*.html'])
+            .pipe(prettify({indent_char: ' ', indent_size: 4}))
+            .pipe(beautify({preserve_newlines: false}))
+            .pipe(gulp.dest('./pretty/'));
 });
 
 gulp.task('beautify', ['beauty', 'cp_all']);
